f3cf4c85db4275cafc1ea6ce40969149a947bcd8
Officeyutong <yt.xyxx@gmail.com>
2023-02-21 02:22:01 +0800

    re-structure directories and update CI (#29)

---
+++ runtime/rust/src/func/map_operate.rs
@@ -0,0 +1,100 @@
+use libbpf_rs::libbpf_sys::{bpf_map_get_next_key, bpf_map_lookup_elem_flags, bpf_map_update_elem, bpf_map_delete_elem_flags, BPF_MAP_GET_NEXT_KEY, BPF_MAP_LOOKUP_ELEM, BPF_MAP_UPDATE_ELEM, BPF_MAP_DELETE_ELEM};
+use log::debug;
+
+use crate::{state::CallerType, func::{ENOENT, EINVAL}, ensure_enough_memory, utils::CallerUtils};
+
+use super::WasmPointer;
+
+pub fn wasm_bpf_map_operate(
+    mut caller: CallerType,
+    fd: i32,
+    cmd: i32,
+    key: WasmPointer,
+    value: WasmPointer,
+    next_key: WasmPointer, // receives the next_key; Since size of key isn't controlled by us, so it's a bit harder to ensure the safety
+    flags: u64,
+) -> i32 {
+    debug!("Map operate");
+    // let mut map = None;
+    let (key_size, value_size, fd) = {
+        let state = caller.data();
+
+        let map = if let Some(v) = state.get_map_by_fd(fd) {
+            v
+        } else {
+            debug!("No map with fd `{}` found", fd);
+            return ENOENT;
+        };
+        (map.key_size() as usize, map.value_size() as usize, map.fd())
+    };
+
+    match cmd as u32 {
+        BPF_MAP_GET_NEXT_KEY => {
+            ensure_enough_memory!(caller, key, key_size, EINVAL);
+            ensure_enough_memory!(caller, next_key, key_size, EINVAL);
+            let ret_val = unsafe {
+                bpf_map_get_next_key(
+                    fd,
+                    caller.raw_pointer_at_unchecked(key as usize) as *const _,
+                    caller.raw_pointer_at_unchecked(next_key as usize) as *mut _,
+                )
+            };
+            if ret_val != 0 {
+                debug!("map get next key failed with {}", ret_val);
+                return ret_val;
+            }
+        }
+        BPF_MAP_LOOKUP_ELEM => {
+            ensure_enough_memory!(caller, key, key_size, EINVAL);
+            ensure_enough_memory!(caller, value, value_size, EINVAL);
+            let ret_val = unsafe {
+                bpf_map_lookup_elem_flags(
+                    fd,
+                    caller.raw_pointer_at_unchecked(key as usize) as *const _,
+                    caller.raw_pointer_at_unchecked(value as usize) as *mut _,
+                    flags,
+                )
+            };
+            if ret_val != 0 {
+                debug!("map lookup elem failed with {}", ret_val);
+                return ret_val;
+            }
+        }
+        BPF_MAP_UPDATE_ELEM => {
+            ensure_enough_memory!(caller, key, key_size, EINVAL);
+            ensure_enough_memory!(caller, value, value_size, EINVAL);
+            let ret_val = unsafe {
+                bpf_map_update_elem(
+                    fd,
+                    caller.raw_pointer_at_unchecked(key as usize) as *const _,
+                    caller.raw_pointer_at_unchecked(value as usize) as *mut _,
+                    flags,
+                )
+            };
+            if ret_val != 0 {
+                debug!("map update elem failed with {}", ret_val);
+                return ret_val;
+            }
+        }
+        BPF_MAP_DELETE_ELEM => {
+            ensure_enough_memory!(caller, key, key_size, EINVAL);
+            let ret_val = unsafe {
+                bpf_map_delete_elem_flags(
+                    fd,
+                    caller.raw_pointer_at_unchecked(key as usize) as *const _,
+                    flags,
+                )
+            };
+            if ret_val != 0 {
+                debug!("map delete elem failed with {}", ret_val);
+                return ret_val;
+            }
+        }
+        // More syscall commands can be allowed here
+        s => {
+            debug!("Map operation `{}` currently not supported", s);
+            return EINVAL;
+        }
+    };
+    return 0;
+}
---
+++ runtime/rust/src/func/wrapper_poll.rs
@@ -0,0 +1,49 @@
+use log::error;
+
+use crate::{func::EINVAL, state::CallerType};
+
+use super::{poll::wasm_bpf_buffer_poll, BpfObjectType, WasmPointer};
+
+pub fn bpf_buffer_poll_wrapper(
+    mut caller: CallerType,
+    program: BpfObjectType,
+    fd: i32,
+    ctx: WasmPointer,
+    data: WasmPointer,
+    max_size: i32,
+    timeout_ms: i32,
+) -> i32 {
+    // let callback_func_name = match caller.data().poll_wrapper {
+    //     PollWrapper::Disabled => {
+    //         panic!("Something terrible happened. bpf_buffer_poll_wrapper must be called with poll_wrapper=PollWrapper::Enabled{{}}");
+    //     }
+    //     PollWrapper::Enabled {
+    //         ref callback_function_name,
+    //     } => callback_function_name.clone(),
+    // };
+    let callback_func_name = caller.data().callback_func_name.clone();
+    caller.data_mut().wrapper_called = true;
+    if let Some(export) = caller.get_export(&callback_func_name) {
+        // if let Some(func) = export.into_func() {
+        //     if let Err(err) = func.typed::<SampleCallbackParams, SampleCallbackReturn>(&mut caller)
+        //     {
+        //         error!(
+        //             "Invalid function signature for {}, expected func(u32, u32, u32) -> u32: {}",
+        //             callback_func_name, err
+        //         );
+        //         return EINVAL;
+        //     }
+        // } else {
+        //     error!("Export {} is not func", callback_func_name);
+        //     return EINVAL;
+        // }
+        if export.into_func().is_none() {
+            error!("Export {} is not func", callback_func_name);
+            return EINVAL;
+        }
+    } else {
+        error!("Callback export named {} not found", callback_func_name);
+        return EINVAL;
+    }
+    wasm_bpf_buffer_poll(caller, program, fd, 0, ctx, data, max_size, timeout_ms)
+}
