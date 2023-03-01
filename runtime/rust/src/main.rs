878a5fb6c05a6512d2020529d5999cd8491d7922
Officeyutong <yt.xyxx@gmail.com>
2023-03-01 18:25:25 +0800

    update: [rust runtime]support passing arguments to Wasm program (#47)

--- runtime/rust/src/main.rs
+++ runtime/rust/src/main.rs
@@ -37,11 +37,12 @@ struct CommandArgs {
     wrapper_module_name: String,
     #[arg(short = 'c', long, help = "Callback export name", default_value_t = String::from("go-callback"))]
     callback_export_name: String,
+    #[arg(help = "Arguments that will be passed to the Wasm program")]
+    args_to_wasm: Vec<String>,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = CommandArgs::parse();
-
     Logger::try_with_str(if args.verbose { "debug" } else { "info" })?
         .format(my_log_format)
         .start()?;
@@ -50,10 +51,15 @@ fn main() -> anyhow::Result<()> {
     let mut linker = Linker::new(&engine);
     wasmtime_wasi::add_to_linker(&mut linker, |s: &mut AppState| &mut s.wasi)
         .with_context(|| anyhow!("Failed to add wasmtime_wasi to linker"))?;
+    let mut args_to_wasm = args.args_to_wasm;
+    args_to_wasm.insert(0, args.wasm_module_file.clone());
+
     let wasi = WasiCtxBuilder::new()
         .inherit_stdio()
         .inherit_args()
         .with_context(|| anyhow!("Failed to build Wasi Context"))?
+        .args(&args_to_wasm[..])
+        .with_context(|| anyhow!("Failed to pass arguments to Wasm program"))?
         .build();
     let mut store = Store::new(&engine, AppState::new(wasi, args.callback_export_name));
     let main_module = Module::from_file(&engine, args.wasm_module_file)
