ae620b17bfa209b83c1fe7e546632b2b070960ab
Littlefisher619 <i@littlefisher.me>
2023-03-07 23:59:00 +0800

    [runtime] add license for rust example

--- runtime/rust/src/func/close.rs
+++ runtime/rust/src/func/close.rs
@@ -1,3 +1,8 @@
+//!  SPDX-License-Identifier: MIT
+//!
+//! Copyright (c) 2023, eunomia-bpf
+//! All rights reserved.
+//!
 use std::collections::hash_map::Entry;
 
 use log::debug;
--- runtime/rust/src/main.rs
+++ runtime/rust/src/main.rs
@@ -1,3 +1,8 @@
+//!  SPDX-License-Identifier: MIT
+//!
+//! Copyright (c) 2023, eunomia-bpf
+//! All rights reserved.
+//!
 use anyhow::{anyhow, Context};
 use clap::Parser;
 use flexi_logger::Logger;
