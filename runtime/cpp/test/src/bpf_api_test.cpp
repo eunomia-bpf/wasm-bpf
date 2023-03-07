76d485eb4392b4b4c31cc891ec61ac87ea4dbdf7
Littlefisher619 <i@littlefisher.me>
2023-03-07 22:33:53 +0800

    runtime: add comment for cpp

--- runtime/cpp/test/src/bpf_api_test.cpp
+++ runtime/cpp/test/src/bpf_api_test.cpp
@@ -1,9 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
  *
- * Copyright (c) 2022, zys
+ * Copyright (c) 2023, eunomia-bpf org
  * All rights reserved.
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
