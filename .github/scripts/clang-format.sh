9df3afac0d24ff8f479f616cd29dbed8715e60e2
officeyutong <yt.xyxx@gmail.com>
2023-03-12 05:32:50 +0000

    update: add lint script

---
+++ .github/scripts/clang-format.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+# Usage: ./clang-format.sh /usr/bin/clang-format(version >= 10.0) [-f]
+# Specify -f to format all c/cxx files, instead of check if the files satisfy the format file
+
+lint() {
+    local targets="examples wasm-sdk runtime/cpp/include runtime/cpp/test runtime/cpp/src"
+    local clang_format="${1}"
+
+    if [ "$#" -ne 1 ]; then
+        echo "please provide clang-format command. Usage ${0} `which clang-format`"
+        exit 1
+    fi
+
+    if [ ! -f "${clang_format}" ]; then
+        echo "clang-format not found. Please install clang-format first"
+        exit 1
+    fi
+    local ext_args="-Werror --dry-run"
+    if [ "${2}" != "-f" ]; then
+        ext_args=""
+    fi
+    find ${targets} -type f -iname *.[ch] -o -iname *.cpp -o -iname *.[ch]xx \
+        | xargs -n1 ${clang_format} -i -style=file ${ext_args}
+
+    exit $?
+}
+
+lint $@
