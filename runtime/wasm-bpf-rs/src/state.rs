4bbe72c9ff4701bb94d593c042bfb4cbd221e74c
officeyutong <yt.xyxx@gmail.com>
2023-03-13 05:40:38 +0000

    remove unnecessary unsafe code

--- runtime/wasm-bpf-rs/src/state.rs
+++ runtime/wasm-bpf-rs/src/state.rs
@@ -40,9 +40,7 @@ pub struct AppState {
     pub wrapper_called: bool,
 }
 
-// We need a seprate thread to monitor the running process of a wasm program, so we must ensure that AppState to be Send
-#[cfg(test)]
-unsafe impl Send for AppState {}
+
 #[allow(unused)]
 struct MyObject {
     pub ptr: *mut libbpf_sys::bpf_object,
