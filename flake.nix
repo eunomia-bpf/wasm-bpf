bc5ca31dedbc21a2395cf8b5467f6d1c63650307
ocfox <i@ocfox.me>
2023-03-25 10:44:45 +0800

    add nix flake support
    Add support for nix flake and direnv,
    
    including devShell and package wasm-bpf

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "wasm-bpf packages and dev shell flake";
+
+  outputs =
+    { self
+    , fenix
+    , flake-utils
+    , naersk
+    , nixpkgs
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      toolchain = with fenix.packages.${system}; combine [
+        minimal.cargo
+        minimal.rustc
+      ];
+      fenix-naersk = naersk.lib.${system}.override {
+        cargo = toolchain;
+        rustc = toolchain;
+      };
+    in
+    {
+      packages = rec {
+        wasm-bpf = fenix-naersk.buildPackage {
+          hardeningDisable = [ "all" ];
+          buildInputs = with pkgs; [
+            zlib
+          ];
+          nativeBuildInputs = with pkgs; [
+            clang
+            elfutils
+            pkg-config
+          ];
+          src = ./runtime;
+        };
+
+        default = wasm-bpf;
+      };
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          toolchain
+          zlib
+          openssl
+        ];
+        nativeBuildInputs = with pkgs; [
+          clang
+          elfutils
+          pkg-config
+        ];
+      };
+    }
+    );
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
+
