00a55d9f8e755b3cfb36c10f7bf743a669f5937f
Officeyutong <yt.xyxx@gmail.com>
2023-06-27 00:45:27 +0800

    Fixes #124 (#125)
    * Remove package lock files
    
    * Committing clang-format changes
    
    * Turn one CargoLock back

--- examples/uprobe/uprobe.bpf.c
+++ examples/uprobe/uprobe.bpf.c
@@ -6,15 +6,13 @@
 char LICENSE[] SEC("license") = "GPL";
 
 SEC("uprobe/./target:uprobe_add")
-int BPF_KPROBE(uprobe_add, int a, int b)
-{
-	bpf_printk("uprobed_add ENTRY: a = %d, b = %d", a, b);
-	return 0;
+int BPF_KPROBE(uprobe_add, int a, int b) {
+    bpf_printk("uprobed_add ENTRY: a = %d, b = %d", a, b);
+    return 0;
 }
 
 SEC("uprobe/./target:uprobe_sub")
-int BPF_KPROBE(uprobe_sub, int a, int b)
-{
-	bpf_printk("uprobed_sub ENTRY: a = %d, b = %d", a, b);
-	return 0;
+int BPF_KPROBE(uprobe_sub, int a, int b) {
+    bpf_printk("uprobed_sub ENTRY: a = %d, b = %d", a, b);
+    return 0;
 }
